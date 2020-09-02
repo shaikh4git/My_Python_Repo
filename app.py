91ec41fcb20e8d531d6d3ad0b0c4fb98a1537fde
shaikh4aws <70565750+shaikh4aws@users.noreply.github.com>
2020-09-02 15:11:06 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+friends = ["Swadiqoon","Swalihoon","Mua'minoon","mualimoon","Aalimoon","Muflihoon"]
+my_kids = ["Laafi","Aashi"]
+print ("My friends are: ")
+for i in friends:
+    print (i)
+print("My kids are: ")
+for k in my_kids:
+    print(k)
+print ("My Wife is:\nShaabi")
+
